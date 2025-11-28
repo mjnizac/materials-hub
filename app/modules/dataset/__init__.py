95c2159cd90f2584489c38289d9178e9c8e303bb
Manuel Jesús Niza Cobo <mannizcob@alum.us.es>
2025-11-28 23:18:38 +0100

    fix: resolver importación circular en dataset module

--- app/modules/dataset/__init__.py
+++ app/modules/dataset/__init__.py
@@ -1,10 +1,12 @@
 from flask_restful import Api
 
-from app.modules.dataset.api import init_blueprint_api
 from core.blueprints.base_blueprint import BaseBlueprint
 
 dataset_bp = BaseBlueprint("dataset", __name__, template_folder="templates")
 
 
 api = Api(dataset_bp)
+
+# Importación diferida para evitar ciclos de importación
+from app.modules.dataset.api import init_blueprint_api
 init_blueprint_api(api)
